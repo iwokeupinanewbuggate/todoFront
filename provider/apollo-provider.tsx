f3d794ee6de5a3ade1c5fc05aa95cd17f7dfeb34
24HP9677 24HP9677 <24HP9677@G303-CC0153.local>
2024-06-19 15:57:38 +0800

    chnaged url

--- provider/apollo-provider.tsx
+++ provider/apollo-provider.tsx
@@ -6,7 +6,7 @@ interface Props extends React.PropsWithChildren {}
 
 const ApolloProviders = ({ children }: Props) => {
   const client = new ApolloClient({
-    uri: " http://localhost:5000/api/graphql",
+    uri: "https://todo-back-murex.vercel.app/api/graphql",
     cache: new InMemoryCache(),
   });
   return (
